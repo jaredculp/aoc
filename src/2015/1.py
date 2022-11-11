662a6f20617651541a34000598d4c5b3542d337b
jaredculp <jculp14@gmail.com>
2022-11-11 13:12:28 -0500

    2015, day 1

---
+++ src/2015/1.py
@@ -0,0 +1,15 @@
+f = open("inputs/2015/1.txt")
+input = f.read()
+
+count = 0
+basement = None
+for i, x in enumerate(input):
+    if count < 0 and not basement:
+        basement = i
+    if x == "(":
+        count += 1
+    elif x == ")":
+        count -= 1
+
+print("part1: ", count)
+print("part2: ", basement)
