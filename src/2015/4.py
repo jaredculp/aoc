092894694c131df314a04eaba0a75d6255e940b2
jaredculp <jculp14@gmail.com>
2022-11-11 14:13:05 -0500

    2015, day 4

---
+++ src/2015/4.py
@@ -0,0 +1,19 @@
+from hashlib import md5
+
+input = open("inputs/2015/4.txt").read().strip()
+
+
+num = 0
+part1 = None
+part2 = None
+while not part1 or not part2:
+    key = f"{input}{num}"
+    hash = md5(key.encode()).hexdigest()
+    if not part1 and hash[:5] == "00000":
+        part1 = num
+    if not part2 and hash[:6] == "000000":
+        part2 = num
+    num += 1
+
+print("part1: ", part1)
+print("part2: ", part2)
