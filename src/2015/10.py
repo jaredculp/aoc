cf967d2ac108dec0c7b9db467f90fddf0845638d
jaredculp <jculp14@gmail.com>
2022-11-15 17:39:55 -0500

    2015, day 10

---
+++ src/2015/10.py
@@ -0,0 +1,28 @@
+input = open("inputs/2015/10.txt").read().strip()
+
+
+def iter(input: str) -> str:
+    output = ""
+    count = 1
+    curr = input[0]
+    for i in input[1:]:
+        if i == curr:
+            count += 1
+        else:
+            output += str(count) + curr
+            curr = i
+            count = 1
+
+    output += str(count) + curr
+    return output
+
+
+for _ in range(40):
+    input = iter(input)
+
+print("part1: ", len(input))
+
+for _ in range(10):
+    input = iter(input)
+
+print("part2: ", len(input))
