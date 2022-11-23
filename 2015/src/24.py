ac83a203c77abbdc5672bd4ab91c2378ab4c0ce2
jaredculp <jculp14@gmail.com>
2022-11-23 14:18:58 -0500

    2015, day 24

---
+++ 2015/src/24.py
@@ -0,0 +1,21 @@
+from functools import reduce
+from itertools import combinations
+from operator import mul
+from typing import Optional
+
+inputs = [int(x.strip()) for x in open("2015/inputs/24.txt").readlines()]
+
+total = sum(inputs)
+
+
+def quantum_entanglement(compartments: Optional[int] = 3):
+    weight = total // compartments
+    cs = []
+    for i in range(len(inputs)):
+        cs = [c for c in combinations(inputs, i) if sum(c) == weight]
+        if cs:
+            return min([reduce(mul, c) for c in cs])
+
+
+print("part1: ", quantum_entanglement())
+print("part1: ", quantum_entanglement(4))
