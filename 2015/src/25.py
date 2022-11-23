8c62e2e073be6505401543b5e72a05f109161688
jaredculp <jculp14@gmail.com>
2022-11-23 15:48:10 -0500

    2015, day 25 part 1 only

---
+++ 2015/src/25.py
@@ -0,0 +1,24 @@
+import re
+
+input = open("2015/inputs/25.txt").readline()
+row, col = [
+    int(x) for x in re.search(r".*row (\d+), column (\d+).", input).groups()
+]
+
+
+i = 20151125
+x, y = 1, 1
+
+while True:
+    if y == 1:
+        y = x + 1
+        x = 1
+    else:
+        y -= 1
+        x += 1
+
+    i = (i * 252533) % 33554393
+
+    if y == row and x == col:
+        print("part1: ", i)
+        break
