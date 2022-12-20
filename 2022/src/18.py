efe3b06f3862066faa4a46d4c57339b41f533dc2
jaredculp <jculp14@gmail.com>
2022-12-20 11:20:33 -0500

    2022, day 18 part 2

--- 2022/src/18.py
+++ 2022/src/18.py
@@ -1,72 +1,37 @@
-from collections import deque
-from functools import lru_cache
-
-inputs = {tuple(map(int, x.split(","))) for x in open("2022/inputs/18.txt")}
-inputs = {
-    tuple(map(int, x.split(",")))
-    for x in """2,2,2
-1,2,2
-3,2,2
-2,1,2
-2,3,2
-2,2,1
-2,2,3
-2,2,4
-2,2,6
-1,2,5
-3,2,5
-2,1,5
-2,3,5
-""".splitlines()
-}
+lava_cubes = {tuple(map(int, x.split(","))) for x in open("2022/inputs/18.txt")}
 
 
-def sides(point):
-    x, y, z = point
-    return [
+def sides(x, y, z):
+    return {
         (x - 1, y, z),
         (x + 1, y, z),
         (x, y - 1, z),
         (x, y + 1, z),
         (x, y, z - 1),
         (x, y, z + 1),
-    ]
+    }
 
 
-p1 = 0
-for i in inputs:
-    for s in sides(i):
-        if s not in inputs:
-            p1 += 1
-
-print(p1)
-
-all_points = [sides(p) for p in inputs]
-min_x = min(p[0] for p in all_points)[0]
-max_x = max(p[0] for p in all_points)[0]
-min_y = min(p[1] for p in all_points)[1]
-max_y = max(p[1] for p in all_points)[1]
-min_z = min(p[2] for p in all_points)[2]
-max_z = max(p[2] for p in all_points)[2]
+def within(x, y, z):
+    return all([min_ <= x <= max_, min_ <= y <= max_, min_ <= z <= max_])
 
 
-@lru_cache(maxsize=None)
-def inside(point):
-    Q = deque()
-    Q.append(point)
-    while Q:
-        p = Q.popleft()
+all_points = [s for c in lava_cubes for s in sides(*c)]
+print(sum(1 if s not in lava_cubes else 0 for s in all_points))
 
-        for x, y, z in sides(p):
-            if (
-                min_x <= x <= max_x
-                and min_y <= y <= max_y
-                and min_z <= z <= max_z
-            ):
-                for s in sides(p):
-                    Q.append(s)
+min_, max_ = min(min(all_points)), max(max(all_points))
 
-    return i
+inside, outside = list(), set()
+Q = [(min_, min_, min_)]
+while Q:
+    point = Q.pop()
+    for side in sides(*point):
+        if side in outside or not all(min_ <= s <= max_ for s in side):
+            continue
 
-
-print(flood((-1, -1, -1)))
+        if side in lava_cubes:
+            inside.append(side)
+        else:
+            outside.add(side)
+            Q.append(side)
+print(len(inside))
