f36b8dc7d499e995cf89820eeb0a421235471db0
jaredculp <jculp14@gmail.com>
2025-12-02 09:23:22 -0500

    fixup! 2025 day2

--- 2025/src/02.py
+++ 2025/src/02.py
@@ -3,6 +3,7 @@ from typing import Iterable
 input = open("2025/inputs/02.txt").read().strip().split(",")
 
 
+# my less elegant solution
 def is_invalid(s: str, sizes: Iterable[int]) -> bool:
     L = len(s)
     return any(n and L % n == 0 and s == s[:n] * (L // n) for n in sizes)
@@ -21,3 +22,16 @@ for line in input:
 
 print(p1)
 print(p2)
+
+# a much more clever way to solve using regex
+# p1 = p2 = 0
+# for line in input:
+#     low, high = line.split("-")
+#     for n in range(int(low), int(high) + 1):
+#         if re.match(r"^(\d+)\1$", str(n)):
+#             p1 += n
+#         if re.match(r"^(\d+)\1+$", str(n)):
+#             p2 += n
+#
+# print(p1)
+# print(p2)
