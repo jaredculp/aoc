1761787d5b3c406a82f486d024c39993d4a22e46
jaredculp <jculp14@gmail.com>
2025-12-04 09:28:06 -0500

    fixup! 2025 day4

--- 2025/src/04.py
+++ 2025/src/04.py
@@ -9,7 +9,7 @@ def find_rolls(rounds=1):
     R = len(G)
     C = len(G[0])
     rolls = set()
-    for r in range(rounds):
+    for _ in range(rounds):
         for r in range(R):
             for c in range(C):
                 if G[r][c] != "@":
@@ -32,3 +32,30 @@ def find_rolls(rounds=1):
 
 print(find_rolls())
 print(find_rolls(100))
+
+
+# a more clever approach which uses set theory
+# calculate the roll locations and intersect with all neighbor rolls
+# you don't even need to do bounds checking on this one, since any
+# neighbor that is out of bounds won't be in the first set
+#
+# def find_rolls(rounds=1):
+#     G = [list(x) for x in input]
+#     R = len(G)
+#     C = len(G[0])
+#     rolls = {(r, c) for r in range(R) for c in range(C) if G[r][c] == "@"}
+#     removed_rolls = set()
+#     for _ in range(rounds):
+#         for r, c in list(rolls): # copy since we modify size in loop
+#             neighbors = {(r + dr, c + dc) for dr, dc in n}
+#             if len(rolls & neighbors) < 4:
+#                 removed_rolls.add((r, c))
+#
+#                 if rounds > 1:
+#                     rolls.remove((r, c))
+#
+#     return len(removed_rolls)
+#
+#
+# print(find_rolls())
+# print(find_rolls(100))
